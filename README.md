c197f9f879ae5001e23eeaf0528b6f7e924c0f50
zhuldyzay23 <zhuldyzay.tleubergen@gmail.com>
2021-02-24 14:50:22 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-or7w8o-cujnmx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-or7w8o-cujnmx)
---
+++ src/app/products.ts
@@ -0,0 +1,133 @@
+export const products = [
+  {
+    id: 1,
+    name:
+      "Soft Landing , Darling Duos, 2-Piece Plush & Premium Sweet Seat Character Chair Bundle, Teal Unicorn",
+    price: 64.99,
+    rating: "4.4 out of 5",
+    description:
+      "Sit back and relax into the comfy arms of a Sweet Seat – and now you can share in the cuddles with your favorite sitting buddy’s darling plush duo!\n" +
+      "A 2-in-1 never looked so cute! This plush and chair bundle makes a perfect photo-op for capturing those oh-so-adorable, picture-perfect moments.\n" +
+      "Tuck your treasures and tech in your new favorite seat’s side pockets, or use the handle to carry from room to room or out the door!\n" +
+      "Complete that bedroom theme or give a gift that will be remembered and loved for years to come!\n" +
+      "Darling Duos are suitable for children 18 months and older.\n",
+    image:
+      "https://m.media-amazon.com/images/I/813UGwhDDHL._AC_UL640_FMwebp_QL65_.jpg",
+    url:
+      "https://www.amazon.com/Soft-Landing-Darling-2-Piece-Character/dp/B08JPB43VD/ref=sr_1_1_sspa?dchild=1&keywords=toy+figures&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=3G2S6A0CCTVHG79XBQSE&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614152491&sr=8-1-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEySDEwVzRTRE9BVURZJmVuY3J5cHRlZElkPUEwODczNjA1M1M0QllHM0tVM1lBOSZlbmNyeXB0ZWRBZElkPUEwNDQ5MTcxM09CV1dFTFFRV0FDRCZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+  },
+  {
+    id: 2,
+    name:
+      " 'Prextex Realistic Looking 7' Dinosaurs Pack of 12 Toys for Boys and Girls 3 Years Old  Up Large Plastic Assorted Dinosaur Figures with Dinosaur Book",
+    price: 15.99,
+    rating: "4.7 out of 5",
+    description:
+      "Prextex 12 Boxed Sizeable 7” dinosaurs Toys for Boys and Girls 3 Years Old & Up Includes Book with history of each dinosaur will inspire imaginative play and curiosity about science \n" +
+      "Kids will enjoy hours of Imaginative Play with realistic-looking dinosaurs\n" +
+      "Fun and educational for both boys and girls alike\n" +
+      "Just right for a school / science project or report\n" +
+      "Ideal party favor or gift for a dinosaur themed birthday party dinosaur toys for kids 3-5 y.o.",
+    image:
+      "https://m.media-amazon.com/images/I/91cmCZxG1hL._AC_UL640_FMwebp_QL65_.jpg",
+    url:
+      "https://www.amazon.com/Prextex-Realistic-Dinosaurs-Assorted-Dinosaur/dp/B011PLVBJG/ref=sxin_9?ascsubtag=amzn1.osa.24cc9435-60f0-443f-8a14-fed26555ceb7.ATVPDKIKX0DER.en_US&creativeASIN=B011PLVBJG&cv_ct_cx=toy+figures&cv_ct_id=amzn1.osa.24cc9435-60f0-443f-8a14-fed26555ceb7.ATVPDKIKX0DER.en_US&cv_ct_pg=search&cv_ct_we=asin&cv_ct_wn=osp-single-source-earns-comm&dchild=1&keywords=toy+figures&linkCode=oas&pd_rd_i=B011PLVBJG&pd_rd_r=5fe2c301-db6b-4ee5-81b3-59f0f123f141&pd_rd_w=7g4KH&pd_rd_wg=cQj1j&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=3G2S6A0CCTVHG79XBQSE&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614152491&sr=1-1-64f3a41a-73ca-403a-923c-8152c45485fe&tag=onsite05f-20"
+  },
+  {
+    id: 3,
+    name: "Peppa Pig Forever Friends Figure 8 Pack",
+    price: 19.99,
+    rating: "5 out of 5",
+    description:
+      "Peppa pig forever friends includes 8 figures Each pair of friends has a matching themed outfit!  Can you match up the pairs in this forever friends 8 piece set?  Have fun with peppa and friends! Peppa & friends are for ages 2+ ",
+    image:
+      "https://m.media-amazon.com/images/I/71pLUCKo3iL._AC_UL640_FMwebp_QL65_.jpg",
+    url:
+      "https://www.amazon.com/Peppa-Pig-Forever-Friends-Figure/dp/B01E1FY0VO/ref=sxin_10_ac_d_pm?ac_md=3-1-QmV0d2VlbiAkMTUgYW5kICQyNQ%3D%3D-ac_d_pm&cv_ct_cx=toy+figures&dchild=1&keywords=toy+figures&pd_rd_i=B01E1FY0VO&pd_rd_r=5fe2c301-db6b-4ee5-81b3-59f0f123f141&pd_rd_w=Xz3mi&pd_rd_wg=cQj1j&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=3G2S6A0CCTVHG79XBQSE&pf_rd_s=merchandised-search-6&pf_rd_t=101&psc=1&qid=1614152491&sr=1-2-22d05c05-1231-4126-b7c4-3e7a9c0027d0"
+  },
+  {
+    id: 4,
+    name: "Tom & Jerry Figure 2-Packs: Play Ball",
+    price: 9.99,
+    rating: "4.6 out of 5",
+    description:
+      "Recreate some of your favorite scenes from the Tom & Jerry movie with these 2 figure packs. Tom & Jerry are ready for the Big League in the Play Ball Tom & Jerry Pack. This 2 figure pack contains 2 accessories - A Baseball Bat and Baseball Glove. There are 4 different 2 Packs to collect so you can bring home the fun of Tom & Jerry! All figures are highly detailed and articulated at the torso and arms​.",
+    image:
+      "https://m.media-amazon.com/images/I/81UZ1ymykPL._AC_UL640_FMwebp_QL65_.jpg",
+    url:
+      "https://www.amazon.com/Tom-Jerry-Figure-2-Packs-Play/dp/B08NWHW8TG/ref=sr_1_14?dchild=1&keywords=toy+figures&pf_rd_i=21439846011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=a8bcad66-2a7f-405d-b0e0-8b0e94ebfd3d&pf_rd_r=3G2S6A0CCTVHG79XBQSE&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1614152491&sr=8-14"
+  },
+  {
+    id: 5,
+    name:
+      "SuSenGo Safari Animal Figures Play Mat - Zoo Park Theme Plastic Toys with Cars, Tiger, Elephant, Lion and Giraffe, Realistic Animals Educational Toys for Kids",
+    price: 24.99,
+    rating: "4.5 out of 5",
+    description:
+      "Safari Animal Figures include a leopard, lion, giraffe, zebra, horse, tiger, elephant, bear, tree*4, cars*2, play mat, fence*6, and an instruction manual. Realistic textures and perfect details bring the animals to life. Come and take a wonderful safari journey with these beautifully detailed animal figures set! Educational Toys: Our toy animals are not only fun toys for kids to play with but also very great educational toys for inspiring kid’s interest in animals and nature and improving kid’s creativity as well as their imagination. Durable Activity Play Mat: The safari theme park play mat has been thickened to make it sturdier, more durable, and comfortable for kids when playing. It is printed clearly and the colors are bright, which will make your children feel like they are on the scene while in their imaginary play. Safe Materials: Safari Animal Figures use PVC material, non-toxic paint, and odorless materials. They’re safe for children and can maintain their color for a very long time. Perfect Gift Choice: A fun and educational toy, it has always been a great gift choice for kids. It’s perfect for Christmas, birthdays, Thanksgiving, New Year, as an impulse gift, children’s bedroom decorations, preschool education, etc.",
+    image: "https://m.media-amazon.com/images/I/61nYd5SNzzL._AC_SR320,320_.jpg",
+    url:
+      "https://www.amazon.com/dp/B08MQ33D2T/ref=sspa_dk_detail_4?psc=1&pd_rd_i=B08MQ33D2T&pd_rd_w=qcshC&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=k2Yrp&pf_rd_r=FATKEZXWAECCGNBF21Y6&pd_rd_r=0fb56c19-60e9-431a-88fa-255b9e232a57&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyVzhDTFgxRE44OTdOJmVuY3J5cHRlZElkPUEwMDgwMDQ5MUNKNTRGS0hRU1JaRyZlbmNyeXB0ZWRBZElkPUEwNjkyMjkzM0M5VkVCTlBSQlNPJndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
+  },
+  {
+    id: 6,
+    name: "Funko Mystery Minis Warner Bros. Classic Cartoons Set",
+    price: 99.99,
+    rating: "4.3 out of 5",
+    description:
+      "$99.99 + No Import Fees Deposit & $58.27 Shipping to Kazakhstan Details Brand	Funko Material	Plastic Number of Pieces	12 About this item Funko Mystery Minis Warner Bros.",
+    image:
+      "https://images-na.ssl-images-amazon.com/images/I/81Fef8xgl9L._AC_UL320_SR320,320_.jpg",
+    url:
+      "amazon.com/Funko-Mystery-Warner-Classic-Cartoons/dp/B077GRGJD6/ref=pd_day0_1?pd_rd_w=HF5UL&pf_rd_p=8ca997d7-1ea0-4c8f-9e14-a6d756b83e30&pf_rd_r=FATKEZXWAECCGNBF21Y6&pd_rd_r=0fb56c19-60e9-431a-88fa-255b9e232a57&pd_rd_wg=k2Yrp&pd_rd_i=B077GRGJD6&psc=1"
+  },
+  {
+    id: 7,
+    name:
+      "TEMI 8 Channels 2.4G Remote Control Dinosaur for Kids Boys Girls, Electronic RC Toys Educational Walking Tyrannosaurus Rex with Lights and Sounds Powered by Rechargeable Battery, 360° Rotation Stunt",
+    price: 59.99,
+    rating: "4 out of 5",
+    description:
+      "High Performance Experience - This dinosaur toy can move forward/backward, turn right/left, shake head, 360 degree rotation stunt with flash light and roar sounds. One Key Demo - At the push of a button, the dinosaur will automatically perform the specific perfect actions. Vivid Lights and Roar Sounds - At the push of a button, the dinosaur can make vivid light and transform several kinds of sounds. Long Playing Time - Equipped with a rechargeable 4.8V,400mAh battery, 2 hours charging time will last up to 15 minutes non-stop operation. With USB charge cable. Safe Material - The RC dinosaur toy is safe for kids and made of sturdy recyclable ABS plastic. Pass the US toy CPSIA and ASTM F963-11 test. ",
+    image: "https://m.media-amazon.com/images/I/515P6SwVdtL._AC_SR320,320_.jpg",
+    url:
+      "https://www.amazon.com/dp/B07DGTSQ21/ref=sspa_dk_detail_3?psc=1&pd_rd_i=B07DGTSQ21&pd_rd_w=UtVm8&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=5AQj4&pf_rd_r=CEKNVED3TT0HNZH609V4&pd_rd_r=1aabeb8f-0def-4366-b26f-78e17c3cd59b&smid=A298XY3U6S0N1Y&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEyUlJLT0tCTzhSRUpKJmVuY3J5cHRlZElkPUEwMDA5NTc3UkZSVTRZQThCWFhWJmVuY3J5cHRlZEFkSWQ9QTAwMzEzOTQzN0o1RzRKQlVHS1M5JndpZGdldE5hbWU9c3BfZGV0YWlsJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
+  },
+  {
+    id: 8,
+    name:
+      "Fisca Remote Control Truck, 1/20 Scale 6 Channel 2.4Ghz RC Dump Truck Construction Vehicle Toy with LED Lights and Simulation Sound for Kids",
+    price: 58.99,
+    rating: "4.3 out of 5",
+    description:
+      "4 Wheel Drive 6CH Fully Functional: Forward, backward, Left, Right, Dump Bed Up and Down. LED Lights and Simulation Sounds: Headlights, Taillights, Top lights. 2.4Ghz High Frequency Remote Control, Automatic Pairing, 10-100M Control Range, Support Several Players to Play Trucks Together at the Same Time without Interference. Made of ABS Plastic with Circumspect and Detailed Design, Just Like The Real Truck, Durable Enough for Indoors and Outdoors Playing. Powered by 4.8V 400mAh Rechargeable Battery (included), 15-20 Minute Playing Time, Transmitter uses 2 AA Batteries (Not Included). If want to buy spare battery pack, ASIN:B076MPGH9P. Spare Charger Power Adapter: B076V2DZ9K. 1:20 Scale,Length: 14 Width: 6 Height: 8, One Key Ignition, Demonstration Function, Long press 3 seconds the Mute Button, the sound effect will be closed, It will not annoy parents while playing.",
+    image: "https://m.media-amazon.com/images/I/51YNLez2CtL._AC_SR320,320_.jpg",
+    url:
+      "https://www.amazon.com/dp/B01N2I1Z4C/ref=sspa_dk_detail_6?psc=1&pd_rd_i=B01N2I1Z4C&pd_rd_w=e9vrx&pf_rd_p=cbc856ed-1371-4f23-b89d-d3fb30edf66d&pd_rd_wg=15fqm&pf_rd_r=TJGSRR5AGQK7N4YHQX46&pd_rd_r=ad8ed15a-e87c-46fc-9853-693fdc4f6a03&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExSlkwTklJNzg1UDNIJmVuY3J5cHRlZElkPUEwOTMwODk4MVAyWEZRMk1LWTRIQyZlbmNyeXB0ZWRBZElkPUEwNTkzODIzMTI0RTBKODVNWEIyUSZ3aWRnZXROYW1lPXNwX2RldGFpbF90aGVtYXRpYyZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+  },
+  {
+    id: 9,
+    name: "Barbie Dreamhouse Dollhouse with Pool, Slide and Elevator",
+    price: 179,
+    rating: "4.8 out of 5",
+    description:
+      "Measuring an impressive 3 feet tall and 4 feet wide and featuring 3 stories, 8 rooms, all-angle play, a working elevator and pool with slide, the Barbie Dreamhouse encourages young imaginations to move into this dollhouse and set up a dream home Dreamy features include the working elevator with room for 4 dolls, a new home office, a carport and a second-story pool with a slide that starts on the third story - fill it with water for a real splash Lights and sounds add realistic touches and encourage role-play: place the frying pan on the stove and press for a sizzling sound; do the same with the tea kettle for whistling; open the oven door to see it light up; turn the oven timer for ticking; and push a button on the toilet for a flushing sound",
+    image:
+      "https://m.media-amazon.com/images/I/81faJqzegWL._AC_UL640_FMwebp_QL65_.jpg",
+    url:
+      "https://www.amazon.com/Barbie-FHY73-DreamHouse/dp/B079JCZMMK/ref=sr_1_6?dchild=1&fst=as%3Aoff&m=ATVPDKIKX0DER&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=ea45a373-9b52-4966-a0a7-59ff9594ea26&pf_rd_r=KWDVZSMFADY9AX1MG1EY&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1580936960&refinements=p_6%3AATVPDKIKX0DER%2Cp_36%3A5000-&rnid=386491011&s=toys-and-games&sr=1-6"
+  },
+  {
+    id: 10,
+    name:
+      "Hot Wheels City Ultimate Garage Track Set with 2 Toy Cars, Garage Playset Features Multi-Level Racetrack, Moving T-Rex Dino & Storage for 100+ 1:64 Scale Vehicles, Toy Gift for Kids 3 Years & Older",
+    price: 99.49,
+    rating: "4.9 out of 5",
+    description:
+      "Inspire hours of imaginative play with an enormous Hot Wheels toy garage, with tons of storage and thrilling racing and stunting challenges! The playset features a vertical tower with parking (and easy storage) for more than 100 Hot Wheels toy cars -- 2 1:64 scale vehicles are included for action right out of the box! Send cars up the elevator -- 2 at a time -- to speed down the side-by-side racetrack from the top of the parking tower for a thrilling downhill challenge with big air jumps! Watch out for the hungry Robo T-Rex looking to devour some tasty Hot Wheels vehicles.",
+    image:
+      "https://m.media-amazon.com/images/I/81KQnfc6MbL._AC_UL640_FMwebp_QL65_.jpg",
+    url:
+      "https://www.amazon.com/Hot-Wheels-GJL14-Ultimate-Garage/dp/B07Y94W69D/ref=sr_1_10?dchild=1&fst=as%3Aoff&m=ATVPDKIKX0DER&pf_rd_i=165793011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=ea45a373-9b52-4966-a0a7-59ff9594ea26&pf_rd_r=KWDVZSMFADY9AX1MG1EY&pf_rd_s=merchandised-search-6&pf_rd_t=101&qid=1580936960&refinements=p_6%3AATVPDKIKX0DER%2Cp_36%3A5000-&rnid=386491011&s=toys-and-games&sr=1-10"
+  }
+];
